33ce7550d98bb0ff3f9a2de71e6ccde26d108d2f
mikey <mikey@hey.com>
2024-04-29 13:41:57 -0500

    fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -4,6 +4,7 @@ const { describe, it, after } = require('mocha');
 chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
+const User = require('../models/user');
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
@@ -18,6 +19,25 @@ describe('Posts', () => {
     url: 'https://www.google.com',
     summary: 'post summary',
   };
+
+  const user = {
+    username: 'poststest',
+    password: 'testposts',
+  };
+
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+
   it('Should create with valid attributes at POST /posts/new', (done) => {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -51,7 +71,23 @@ describe('Posts', () => {
       });
   });
 
-  after(() => {
-    Post.findOneAndDelete(newPost);
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+      .then(function () {
+        agent.close();
+
+        User.findOneAndDelete({
+          username: user.username,
+        })
+          .then(function () {
+            done();
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      })
+      .catch(function (err) {
+        done(err);
+      });
   });
 });
